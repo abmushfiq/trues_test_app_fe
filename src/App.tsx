686647f9787a6ffb5ed09363d0ffed02b26fd677
abmushfiq <abmushfiq@gmail.com>
2023-08-12 21:18:59 +0530

    test-directly

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <h1>Hi iam React pipeline test pr ammazing test</h1>
+=======
+      <h1>Hi iam React pipeline test directly</h1>
+>>>>>>> d746417 (test-directly)
     </div>
   );
 }
