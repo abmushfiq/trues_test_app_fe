0efae16a99cc0c3d68e79db93e813be530bbb4b1
abmushfiq <abmushfiq@gmail.com>
2023-08-12 20:51:26 +0530

    test-pr2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hi iam React pipeline test pr ammazing</h1>
+      <h1>Hi iam React pipeline test pr ammazing test</h1>
     </div>
   );
 }
